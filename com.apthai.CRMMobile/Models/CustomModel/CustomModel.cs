bd2b8f43cba1cbc0ac5dea99f8afb4a4a5f15124
Pacharavach Vacharakittanond <pacharavach_v@apthai.com>
2020-04-07 14:39:52 +0700

    deploy

--- com.apthai.CRMMobile/Models/CustomModel/CustomModel.cs
+++ com.apthai.CRMMobile/Models/CustomModel/CustomModel.cs
@@ -152,6 +152,9 @@ namespace com.apthai.CRMMobile.CustomModel
         public string AgreementOwnerAddressEN { get; set; }
         public string BookingOwnerEmail { get; set; }
         public string AgreementOwnerEmail { get; set; }
+        public string ProjectAddressTH { get; set; }
+        public string ProjectAddressEN { get; set; }
+        public string PhoneNumber { get; set; }
     }
     public class iCRMContact
     {
