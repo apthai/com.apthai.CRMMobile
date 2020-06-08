34d3053740a0bdb6624d33c0d00018158b7e3a60
Pacharavach Vacharakittanond <pacharavach_v@apthai.com>
2020-06-08 09:59:52 +0700

    deploy

--- com.apthai.CRMMobile/Startup.cs
+++ com.apthai.CRMMobile/Startup.cs
@@ -74,8 +74,7 @@ namespace com.apthai.CRMMobile
             Configuration = builder.Build();
 
         }
-
-
+        
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
